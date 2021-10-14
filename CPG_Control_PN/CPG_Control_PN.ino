03d503299dafdd7cc1b988b6851a0fb3ee383af3
Pascal <p.anema@student.maastrichtuniversity.nl>
2021-10-14 12:54:44 +0200

    aagh

--- CPG_Control_PN/CPG_Control_PN.ino
+++ CPG_Control_PN/CPG_Control_PN.ino
@@ -4,24 +4,29 @@
 //Calibration of Servo Limits
 //Please enter our specific servo limits for servo 1,2, (and 3) here
 //You can find the corresponding limits in your EDMO box
-int SERVOMIN[]  {100, 104, 105}; //PLEASE ENTER: The lower motor PPM limit (Servo Min) as noted in your EDMO box
-int SERVOMAX[]  {456, 450, 465}; //PLEASE ENTER: The upper motor PPM limit (Servo Max) as noted in your EDMO box
+int SERVOMIN[]  {100, 104, 106, 100}; //PLEASE ENTER: The lower motor PPM limit (Servo Min) as noted in your EDMO box
+int SERVOMAX[]  {456, 450, 454, 442}; //PLEASE ENTER: The upper motor PPM limit (Servo Max) as noted in your EDMO box
 
-int POTIMIN[]  {118, 145, 130}; //PLEASE ENTER: The lower potentiometer limit (Poti Low) as noted in your EDMO box
-int POTIMAX[]  {694, 747, 710}; //PLEASE ENTER: The upper potentiometer limit (Poti High) as noted in your EDMO box
+int POTIMIN[]  {118, 145, 142, 130}; //PLEASE ENTER: The lower potentiometer limit (Poti Low) as noted in your EDMO box
+int POTIMAX[]  {694, 747, 720, 709}; //PLEASE ENTER: The upper potentiometer limit (Poti High) as noted in your EDMO box
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////// 
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-#include "study.h"
+//#include "study.h"
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
+<<<<<<< Updated upstream
 const unsigned int NUM_OSCILLATORS = 3; // this number has to match entries in array osc[] (do NOT modify!!)
+=======
+const int NUM_OSCILLATORS = 4; // this number has to match entries in array osc[] (do NOT modify!!)
+// third motor wires are not connected, so we are limited to two oscilators in reality
+>>>>>>> Stashed changes
 
 //Poti variables
 int POTIPINS[] = {14, 15, 16, 17, 18, 19}; //Do NOT modify!! Analog pins to which the potis' of the servos are connected (A0=14, A1=15, A2=16, ...)
-float poti_value[] = {0, 0, 0}; //Do NOT modify! Array to store raw poti values
-int servo_angle[] = { 0, 0, 0}; //Do NOT modify! Array to store mapped poti values
+float poti_value[] = {0, 0, 0, 0}; //Do NOT modify! Array to store raw poti values
+int servo_angle[] = { 0, 0, 0, 0}; //Do NOT modify! Array to store mapped poti values
 
 //Timing variables
 unsigned long previousMillis = 0;
@@ -30,9 +35,16 @@ unsigned long timeStep = 10; // period used to update CPG state variables and se
 int interval = 0; //variable to store actual measured update time of the PID
 
 //CPG parameter
+<<<<<<< Updated upstream
 double frequency = 0.5; // oscillator frequency
 double rateOfFrequency = 0;
 double targetFrequency = 0.5;
+=======
+double frequency = 0; // oscillator frequency; the current frequency, which slowly goes to target frequency when that changes
+double gain_frequency = 1; // Adaptation rate for frequency, we replaced c by this variable and gain_offset
+double rateOfFrequency = 0; // derivative of the frequency (can be used for plotting)
+double targetFrequency = 0;
+>>>>>>> Stashed changes
 
 double w = 0.025; // we assume that all oscillators use same coupling weight
 double a = 1; // we assume that all oscillators use same adaptation rate for amplitude
@@ -59,9 +71,16 @@ typedef struct
 // initalisation with offset 90 for all motors (since servos operate in the range 0-180)
 oscillator osc[NUM_OSCILLATORS] = 
 {
+<<<<<<< Updated upstream
     {0,30,30,90,90,0,0,0,0,0,{0,PI,0},{0,1,0}},
     {0,30,30,90,90,0,0,0,0,0,{-PI,0,PI},{1,0,1}},
     {0,30,30,90,90,0,0,0,0,0,{0,-PI,0},{0,1,0}}
+=======
+    {0,30,30,90,90,0,0,0,0,0,{0,PI,0,0},{0,1,0,0}},
+    {0,30,30,90,30,0,0,0,0,0,{-PI,0,0,0},{1,0,1,0}},
+    {0,30,30,90,90,0,0,0,0,0,{0,0,0,PI},{0,1,0,1}},
+    {0,30,30,90,90,0,0,0,0,0,{0,0,-PI,0},{0,0,1,0}}
+>>>>>>> Stashed changes
 };
 
 // strings for reading input
@@ -76,9 +95,9 @@ void setup()
     Serial.flush();//(do NOT modify!!)
     while (!Serial); ////(do NOT modify!!) wait for the USB to serial chip to get ready (really quick!!)Does NOT! wait for the serial monitor to open!!!!
     ///////Initialize SD-Card /////////////////////
-    initSD();//(do NOT modify!!)
+//    initSD();//(do NOT modify!!)
     ///////Initialize Real time clock ////////////   
-    initRTC(); //(do NOT modify!!)         
+//    initRTC(); //(do NOT modify!!)         
     ///////Motor control/////////////////////////////////
     pwm.begin();
     pwm.setPWMFreq(50);  // Analog servos run at ~60 Hz update
@@ -95,7 +114,7 @@ void loop(){
     currentMillis = millis(); //update the current time (do NOT modify!!)
     if (currentMillis - previousMillis >= timeStep){//CPG update interval (10ms)(do NOT modify!!)
       interval=currentMillis - previousMillis; //calculate actual interval time (do NOT modify!!)
-      saveTime(interval); //saves current interval time (do NOT modify!!)
+//      saveTime(interval); //saves current interval time (do NOT modify!!)
       previousMillis = currentMillis; //update the previous time step (do NOT modify!!)
       readInput(); //read input command from serial monitor (do NOT modify!!)
       
@@ -125,9 +144,9 @@ void loop(){
       //+++++++++++++++IMPLEMENT your CPG control code here ABOVE!!!++++++++++++++++++++++++++++++++++++
       Serial.println();
       for(int t=0;t<NUM_OSCILLATORS;t++){//(do NOT modify!!)
-        savePose(osc[t].pos,servo_angle[t]);//saves positions to sd-buffer (do NOT modify!!)
+//        savePose(osc[t].pos,servo_angle[t]);//saves positions to sd-buffer (do NOT modify!!)
       }
-      saveBuffer();//upload buffer to sd (do NOT modify!!)
+//      saveBuffer();//upload buffer to sd (do NOT modify!!)
     }
 }
 
@@ -149,9 +168,15 @@ void readInput()
     //if there is an input via the serial monitor read and parse it
     if (Serial.available()) 
     {
+<<<<<<< Updated upstream
       spaceCount = 0;
       recFlag =1;
       commandString = Serial.readStringUntil('\n'); //read received string 
+=======
+      int spaceCount = 0;
+      int recFlag =1;
+      commandString = Serial.readStringUntil('\n'); //read received string
+>>>>>>> Stashed changes
       /////////////////PARSE INPUT//////////////////////////////////////////
       //count the number of spaces in the string 
       for(int i=0; i<=commandString.length();i++){
@@ -169,11 +194,19 @@ void readInput()
             osc[(int) indexString.toInt()].targetAmplitude = (int) valueString.toInt();
           }else if(commandString.startsWith("off")){
             osc[(int) indexString.toInt()].targetOffset = (int) valueString.toInt();
+<<<<<<< Updated upstream
           } 
           //save received command string to buffer    
           buffer += commandString;
           buffer += " ";
           buffer += "-";
+=======
+          }
+          //save received command string to buffer
+//          buffer += commandString;
+          //buffer += " ";
+          //buffer += "-";
+>>>>>>> Stashed changes
           recFlag =0;
       }else if (recFlag ==1 && spaceCount == 1 && (commandString.startsWith("freq") || (commandString.startsWith("weight")))){ 
           // change the target frequency for all oscillators
@@ -184,6 +217,7 @@ void readInput()
             valueString = commandString.substring(7, commandString.length());
             w = (float) valueString.toFloat();
           }
+<<<<<<< Updated upstream
           //save received command string to buffer  
           buffer += commandString;
           buffer += " ";  
@@ -192,6 +226,16 @@ void readInput()
           buffer += "-";
           recFlag =0;        
       } else if (recFlag ==1 && spaceCount == 3 && commandString.startsWith("phb")){ 
+=======
+          //save received command string to buffer
+          /*buffer += commandString;
+          buffer += " ";
+          buffer += "-";
+          buffer += " ";
+          buffer += "-";*/
+          recFlag =0;
+      } else if (recFlag ==1 && spaceCount == 3 && commandString.startsWith("phb")){
+>>>>>>> Stashed changes
           // change the phase bias between the two specified oscillators
           indexString = commandString.substring(4, 5);
           int index1 = (int) indexString.toInt();
@@ -200,6 +244,7 @@ void readInput()
           valueString = commandString.substring(8, commandString.length());
           osc[index1].phaseBias[index2] =  ((PI * valueString.toFloat())/180);//deg2rad!!! (float)
           osc[index2].phaseBias[index1] = -osc[index1].phaseBias[index2];
+<<<<<<< Updated upstream
           //save received command string to buffer  
           buffer += commandString;
           recFlag =0;      
@@ -212,6 +257,20 @@ void readInput()
           buffer += "-"; 
           buffer += " "; 
           buffer += "-";
+=======
+          //save received command string to buffer
+          //buffer += commandString;
+          recFlag =0;
+      } else if (recFlag ==1 && spaceCount == 0 && commandString.startsWith("print")){
+          //save received command string to buffer
+//          buffer += commandString;
+//          buffer += " ";
+          /*buffer += "-";
+          buffer += " ";
+          buffer += "-";
+          buffer += " ";
+          buffer += "-";*/
+>>>>>>> Stashed changes
           recFlag =0;
           // print information about the current state of the oscillators
           Serial.print("Frequency: ");
@@ -249,24 +308,37 @@ void readInput()
           }
         //in case none of the above input variations is correct it is declared as an invalid input
         }else if (recFlag ==1){
+<<<<<<< Updated upstream
           //save received command string to buffer  
           buffer += "Invalid Input";
           buffer += " "; 
           buffer += "-"; 
           buffer += " "; 
           buffer += "-";
+=======
+          //save received command string to buffer
+          /*buffer += "Invalid Input";
+          buffer += " ";
+          buffer += "-";
+          buffer += " ";
+          buffer += "-";*/
+>>>>>>> Stashed changes
           recFlag =0;
         }
     //if there is NO new input that has been received via the serial monitor, fill the empty spots in the .txt file with dashes
     }else{
-      buffer += "-";
+      /*buffer += "-";
       buffer += " ";
       buffer += "-";
       buffer += " ";  
       buffer += "-";
+<<<<<<< Updated upstream
       buffer += " "; 
       buffer += "-"; 
       buffer += " "; 
+=======
+      buffer += " ";*/
+>>>>>>> Stashed changes
     }
 }
 
